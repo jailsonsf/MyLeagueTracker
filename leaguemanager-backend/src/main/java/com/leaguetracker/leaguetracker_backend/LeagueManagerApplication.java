891a2dfd4a0dba8b35c060ce7bfe3fb1c061e606
jailsonsf <jailson.s.f@outlook.com>
2026-02-17 12:29:46 -0300

    Create spring project

---
+++ leaguemanager-backend/src/main/java/com/leaguetracker/leaguetracker_backend/LeagueManagerApplication.java
@@ -0,0 +1,13 @@
+package com.leaguetracker.leaguetracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueManagerApplication.class, args);
+	}
+
+}
---
+++ leaguemanager-backend/src/test/java/com/leaguetracker/leaguetracker_backend/LeaguetrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaguetracker.leaguetracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaguetrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
